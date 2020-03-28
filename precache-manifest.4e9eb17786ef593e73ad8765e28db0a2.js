f98c02022e9db9dd0c320ab514c8c10395e079e6
Grzegorz Paprzycki <paprzycki.grzegorz@gmail.com>
2020-03-28 15:27:18 +0100

    Updates

---
+++ precache-manifest.4e9eb17786ef593e73ad8765e28db0a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "799cf18a9de5c9438bd6",
+    "url": "/ToDo-App_React/static/css/main.9ec92897.chunk.css"
+  },
+  {
+    "revision": "799cf18a9de5c9438bd6",
+    "url": "/ToDo-App_React/static/js/main.799cf18a.chunk.js"
+  },
+  {
+    "revision": "f6b84cc0c44b8101ab3f",
+    "url": "/ToDo-App_React/static/js/1.f6b84cc0.chunk.js"
+  },
+  {
+    "revision": "29346e0052b89d39bb05",
+    "url": "/ToDo-App_React/static/js/runtime~main.29346e00.js"
+  },
+  {
+    "revision": "e9bde419d528420488f850b0c7a568d5",
+    "url": "/ToDo-App_React/index.html"
+  }
+];
